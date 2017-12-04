9b740b20fdb1d270b3ba8b19b10087b3422d4dba
Mikhail Swift <mikhail.swift@gmail.com>
2017-12-03 21:45:09 -0500

    VSCode Builds

--- game/src/build-win32-vscode.bat
+++ game/src/build-win32-vscode.bat
@@ -1,10 +1,10 @@
 call "D:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 
-IF NOT EXIST ..\build MKDIR ..\build
-PUSHD ..\build
+IF NOT EXIST game\build MKDIR game\build
+PUSHD game\build
 IF NOT EXIST .\win32 MKDIR win32
 PUSHD win32
-dir
+
 cl /MD /Z7 /FC /nologo ..\..\src\main.c /I ..\..\src\include /link -incremental:no ..\..\src\libs\win32\msvc\raylib.lib ..\..\src\libs\win32\msvc\glfw3.lib ..\..\src\libs\win32\msvc\OpenAL32.lib gdi32.lib user32.lib shell32.lib
 
 PUSHD ..\..\src
